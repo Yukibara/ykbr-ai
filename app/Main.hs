3b691d113121c33afb6c88293b0088500e88bc56
Yukibara <nitic.mtbr@gmail.com>
2018-10-31 11:39:00 +0900

    諸々を直書きしているので悪い ツイートは出ない

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
-
-main :: IO ()
-main = someFunc
+    import Lib
+    import qualified Data.Text.IO as T
+    
+    main :: IO ()
+    main = do
+      tweet "テストですよ、聞こえますか"
